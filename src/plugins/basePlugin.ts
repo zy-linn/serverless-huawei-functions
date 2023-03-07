86f2148a3797114aea8c2b5f96152f9ea4f0b7f9
zy-linn <120696300+zy-linn@users.noreply.github.com>
2023-03-07 15:27:58 +0800

    Create basePlugin.ts

---
+++ src/plugins/basePlugin.ts
@@ -0,0 +1,28 @@
+import _ from "lodash";
+import Serverless, { Options } from "serverless";
+import { ServerlessHookMap } from "../models/serverless";
+import Provider from "../provider";
+import { FunctionService } from "../services/function.service";
+
+export class BasePlugin {
+    public hooks: ServerlessHookMap;
+
+    private provider: Provider;
+
+    public constructor(protected serverless: Serverless, protected options: Options) {
+        this.provider = (this.serverless.getProvider(Provider.getProviderName()) as any);
+    }
+
+    async getIns(): Promise<FunctionService[]> {
+        const client = await this.provider.getFgClient();
+        const { functions, provider = {} } = this.serverless.service;
+        const basicConfig = _.omit(provider, ['name', 'credentials', 'stage']);
+        return Object.keys(functions).map(key => {
+            return new FunctionService(client.getFunctionClient(), {
+                ...basicConfig,
+                ...functions[key],
+                projectId: client.getProjectId
+            });
+        });
+    }
+}
