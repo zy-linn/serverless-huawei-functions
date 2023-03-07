862fc8ea733fbe0ce09ef8913a250b09685e5339
zy-linn <120696300+zy-linn@users.noreply.github.com>
2023-03-07 20:10:11 +0800

    Update deploy.ts

--- test/fixtures/deploy.ts
+++ test/fixtures/deploy.ts
@@ -4,7 +4,38 @@ import HuaweiIndex from '../../src/index';
 (async () => {
     try {
         const serverless: any = MockFactory.createTestServerless({
-            configuration: { },
+            configuration: { 
+            service: "fg-nodejs",
+                frameworkVersion: "3",
+                provider: {
+                    name: "huawei",
+                    runtime: "Node.js14.18",
+                    region: 'cn-north-4',
+                    memorySize: 256,
+                    timeout: 10,
+                    credentials: "~\\.fg\\credentials",
+                },
+                plugins: [
+                    "serverless-aliyun-function-compute",
+                ],
+                functions: {
+                    hello: {
+                        handler: "index.handler",
+                        description: "Tencent Serverless Cloud Function",
+                        memorySize: 256,
+                        timeout: 10,
+                        environment: {
+                            variables: {
+                                ENV_FIRST: "env1",
+                                ENV_Third: "env2",
+                            },
+                        },
+                    },
+                    world: {
+                        handler: "index.handler1",
+                    },
+                },
+            },
             serviceDir: "",
             configurationFilename: "serverless.yml",
             commands: [
